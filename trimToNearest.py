83f5c85e37ea3cd8130fdb9b538c9a9f29fa693d
Jeltje van Baren <jeltje.van.baren@gmail.com>
2016-03-28 19:05:30 -0700

    first commit of incomplete trim script

---
+++ trimToNearest.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+
+import sys, os, re, getopt
+import argparse
+from bisect import bisect_left
+
+usage = sys.argv[0]+"""
+
+Trims input CNV start and end coordinates to match nearest
+marker in markerfile. This creates GISTIC input.
+
+
+"""
+def build_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--targets', required=True, help='exome targets file in bed format')
+    parser.add_argument('-l', '--inlist', required=True, help='list of cnv locations')
+    parser.add_argument('-m', '--markerout', required=True, help='name of output markerfile')
+    return parser
+
+def bedToMarkers(bedfile, markerObj, outputfile):
+    """
+    Converts gistic input format markers file from a bed format input
+    """
+    # bed is chrom, start, end, id, other fields
+    # must remove chr from chrom
+    # markersfile is id, chrom, pos. We need the start and end from the bed input on separate lines
+
+    id = 0
+    with open(outputfile, 'w') as w:
+        with open(bedfile, 'r') as f:
+            for line in f.readlines():
+                sample = line.strip().split('\t')
+                (chrom, start, end), rest = sample[:3], sample[3:]
+                if chrom.startswith('chr'):
+                    chrom = chrom[3:]
+            markerObj.add(chrom, start)
+            markerObj.add(chrom, end)
+            id += 1
+            w.write("s{}\t{}\t{}".format(id, chrom, start)
+            id += 1
+            w.write("e{}\t{}\t{}".format(id, chrom, end)
+
+
+class markers(object):
+    def __init__(self):
+        self.chromdict = dict()  
+    def add(self, chrom, coord):
+        try:
+            self.chromdict[chrom].append(int(coord))
+        except KeyError:
+            self.chromdict[chrom] = [int(coord)]
+    def sort(self):
+        for chr in self.chromdict:
+            self.chromdict[chr].sort()
+
+    def trim(self, cnv):
+        """Trim input start and end coordinate to nearest chromosome coordinate"""
+        cfields = cnv.split("\t")
+        new = takeClosest(self.chromdict[cfields[1]], int(cfields[2]), "right")
+        cfields[2] = new
+        new = takeClosest(self.chromdict[cfields[1]], int(cfields[3]), "left")
+        cfields[3] = new
+        if cfields[2] > cfields[3]:
+             print >>sys.stderr, "skipping", cnv
+             return None
+	return ("\t").join(str(x) for x in cfields)
+
+def takeClosest(myList, myNumber, side):
+    """
+    Assumes myList is sorted. Returns closest value to myNumber on submitted side.
+
+    If two numbers are equally close, return the smallest number.
+    """
+    pos = bisect_left(myList, myNumber)
+    if side == "left":
+        return myList[pos-1]
+    if side == "right":
+        return myList[pos]
+
+
+# Main
+# Run program
+parser = build_parser()
+args = parser.parse_args()
+
+# Create markers object
+markerlist = markers()
+
+# Create markers and marker output from input bed file
+bedToMarkers(args.targets, markerlist, args.markerout)
+markerlist.sort()
+
+# Now read in list of cnv files and trim them
+
+
+# now parse CNV
+
+f = open(args[1],'r')
+
+for line in f:
+    outline = markerlist.trim(line.strip()) 
+    if outline:
+        print outline
+f.close()
+
+
